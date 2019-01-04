5283503df8a09141d0bb133f0345f883c8180c0a
浮生若梦 <microease@163.com>
2019-01-04 12:21:54 +0800

    add

---
+++ Chapter 1/1.9.py
@@ -0,0 +1,21 @@
+# 怎样在两个字典中寻寻找相同点（比如相同的键、相同的值等等）？
+a = {
+    'x': 1,
+    'y': 2,
+    'z': 3,
+}
+
+b = {
+    'w': 10,
+    'x': 11,
+    'y': 2,
+}
+# 找到2个字典中相同的key
+print(a.keys() & b.keys())
+# 找到在a字典中不在b字典中的key
+print(a.keys() - b.keys())
+# 找到2个字典中key和value都相同的元素
+print(a.items() & b.items())
+# 修改或过滤字典元素
+c = {key:a[key] for key in a.keys() - {'z','w'}}
+print(c)
