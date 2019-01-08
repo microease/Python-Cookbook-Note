5206104003f286b204c68db3661df18d923c1ca7
浮生若梦 <microease@163.com>
2019-01-08 10:51:45 +0800

    add

--- Chapter_2/2.20.py
+++ Chapter_2/2.20.py
@@ -1,3 +1,7 @@
 # 你想在字节字符串上执行普通的文本操作 (比如移除，搜索和替换)。
 data = b'hello world'
 print(data[0:5])
+print(data.startswith(b'hello'))
+print(data.replace(b'hello',b'hello cruel'))
+data = bytearray(b'hello world')
+print(data[0:5])
