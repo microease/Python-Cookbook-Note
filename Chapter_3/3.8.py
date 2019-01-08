6b6dbb1e2b65bfa2d1cca9ea47f1dbda761f26b3
microease <microease@163.com>
2019-01-08 22:41:14 +0800

    add

---
+++ Chapter_3/3.8.py
@@ -0,0 +1,8 @@
+# 你进入时间机器，突然发现你正在做小学家庭作业，并涉及到分数计算问题。或者 你可能需要写代码去计算在你的木工工厂中的测量值。
+
+from fractions import Fraction
+
+a = Fraction(5, 4)
+b = Fraction(7, 16)
+print(a + b)
+print(a * b)
---
+++ Chapter_3/3.9.py
@@ -0,0 +1,12 @@
+# 你需要在大数据集 (比如数组或网格) 上面执行计算。
+x = [1, 2, 3, 4]
+y = [5, 6, 7, 8]
+print(x * 2)
+# print(x + 10)
+print(x + y)
+import numpy as np
+
+ax = np.array([1, 2, 3, 4, 5])
+ay = np.array([6, 7, 8, 9, 10])
+print(ax * 2)
+print(ax + ay)
