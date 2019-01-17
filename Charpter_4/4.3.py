ce1336b805ac9ce45864eb070d9cae41287a9f6e
浮生若梦 <microease@163.com>
2019-01-17 17:32:32 +0800

    add

--- Charpter_4/4.3.py
+++ Charpter_4/4.3.py
@@ -1,12 +1,13 @@
 # 你想实现一个自定义迭代模式，跟普通的内置函数比如 range() , reversed() 不
 # 一样。
-def frange(start,stop,increment):
+def frange(start, stop, increment):
     x = start
     while x < stop:
         yield x
         x += increment
 
-for n in frange(0,4,0.5):
+
+for n in frange(0, 4, 0.5):
     print(n)
 
-print(list(frange(0,4,0.5)))
+print(list(frange(0, 4, 0.5)))
--- Charpter_4/4.4.py
+++ Charpter_4/4.4.py
@@ -1,5 +1,25 @@
 # 你想构建一个能支持迭代操作的自定义对象，并希望找到一个能实现迭代协议的
 # 简单方法。
 class Node:
-    def __init__(self,value):
+    def __init__(self, value):
         self._value = value
+        self._children = []
+
+    def __repr__(self):
+        return "Node({!r})".format(self._value)
+
+    def __iter__(self):
+        return iter(self._children)
+
+    def depth_first(self):
+        yield self
+        for c in self:
+            yield from c.depth_first()
+
+
+if __name__ == "__main__":
+    root = Node(0)
+    child1 = Node(1)
+    child2 = Node(2)
+    root.add_child(child1)
+    root.add_child(child2)
